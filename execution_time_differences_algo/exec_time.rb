2b20eeab9f7514c052f929a84adda2e3ec758fca
Andrew Kim <andrewikim95@gmail.com>
2021-01-22 14:11:28 -0500

    Finished #largest_con_sums

--- execution_time_differences_algo/exec_time.rb
+++ execution_time_differences_algo/exec_time.rb
@@ -26,5 +26,31 @@ end
 puts Benchmark.measure {
     p my_min2(list)
 }
-
-
+list1 = [5, 3, -7]
+def largest_con_sums(list)
+    sub_arrs = []
+    list.each_index do |idx1|
+        list.each_index do |idx2|
+            sub_arrs << list[idx1..idx2]
+        end
+    end
+    (sub_arrs.map {|arr| arr.sum }).max
+end
+puts Benchmark.measure {
+    p largest_con_sums(list1)
+}
+list2 = [5, 3, -7]
+def largest_con_sums2(list)
+    largest_sum = 0
+    current_sum = 0
+    list.each do |ele|
+        current_sum += ele
+        if largest_sum < current_sum
+            largest_sum = current_sum
+        end
+    end
+    largest_sum
+end
+puts Benchmark.measure {
+    p largest_con_sums2(list2)
+}
